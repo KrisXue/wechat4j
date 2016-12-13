ffbeefbfcd65bbcd322ed142269091e629354049
Sugar <Sugar@Sugar-PC>
2016-12-13 15:35:22 +0800

    添加卡券

---
+++ src/org/sword/wechat4j/card/WxCardGeneralCoupon.java
@@ -0,0 +1,29 @@
+package org.sword.wechat4j.card;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author jackylian
+ */
+public class WxCardGeneralCoupon extends WxCard
+{
+    public WxCardGeneralCoupon()
+    {
+        init("GENERAL_COUPON");
+    }
+    
+    public void setDefaultDetail(String detail)
+    {
+        m_data.put("default_detail", detail);
+    }
+    
+    public String getDefaultDetail()
+    {
+        return m_data.optString("default_detail");
+    }
+}
---
+++ src/org/sword/wechat4j/card/WxCardGroupon.java
@@ -0,0 +1,26 @@
+package org.sword.wechat4j.card;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author jackylian
+ */
+public class WxCardGroupon extends WxCard
+{
+
+    public WxCardGroupon()
+    {
+        init("GROUPON");
+    }
+    
+    public void setDealDetail(String detail)
+    {
+        m_data.put("deal_detail", detail);
+    }
+    
+}
